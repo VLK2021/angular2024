aab6c7d361a8ae75782ec3bd9e2cc3a3956fb06a
vlk2022 <volodymyrkostiuk2021@gmail.com>
2024-10-10 14:53:14 +0300

    start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {HeaderComponent} from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
-  template: '<router-outlet></router-outlet>',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'angular2024';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,20 +1,4 @@
 import { Routes } from '@angular/router';
-import {HomeComponent} from './pages/home/home.component';
-import {UsersComponent} from './pages/users/users.component';
-import {MaineLayoutComponent} from './layouts/maine-layout/maine-layout.component';
-import {UserDetailsComponent} from './pages/user-details/user-details.component';
 
 export const routes: Routes = [
-  {
-    path: '', component: MaineLayoutComponent, children: [
-      {path: '', redirectTo: 'home', pathMatch: 'full'},
-      {path: 'home', component: HomeComponent},
-      {
-        path: 'users', component: UsersComponent, children: [
-          {path: ':id', component: UserDetailsComponent}
-        ]
-      },
-      { path: '**', redirectTo: '', pathMatch: 'full' }
-    ]
-  }
 ];
