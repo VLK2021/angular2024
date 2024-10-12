5bc2393ecd1406b1ca1308905fe82e08d7a3fa29
vlk2022 <volodymyrkostiuk2021@gmail.com>
2024-10-12 10:47:14 +0300

    BehaviorSubject version 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {AComponent} from './components/a/a.component';
+import {BComponent} from './components/b/b.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AComponent, BComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/a/a.component.ts
@@ -0,0 +1,23 @@
+import {Component, OnInit} from '@angular/core';
+import {CounterService} from '../../services/counter.service';
+
+@Component({
+  selector: 'app-a',
+  standalone: true,
+  imports: [],
+  templateUrl: './a.component.html',
+  styleUrl: './a.component.css'
+})
+export class AComponent implements OnInit {
+  count: number | undefined
+
+  constructor(private countService: CounterService) {
+  }
+
+  ngOnInit(): void {
+    this.countService.getCount().subscribe(value => {
+      this.count = value
+    })
+  }
+
+}
