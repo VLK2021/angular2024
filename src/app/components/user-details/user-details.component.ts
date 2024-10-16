cb5a9ab6f41a45dc4d491ee68ae7edf372649b21
vlk2022 <volodymyrkostiuk2021@gmail.com>
2024-10-16 11:59:37 +0300

    resolver

--- src/app/components/user-details/user-details.component.ts
+++ src/app/components/user-details/user-details.component.ts
@@ -1,7 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {UserInterfaces} from '../../interfaces/user-interfaces';
+
 import {ActivatedRoute, Router} from '@angular/router';
-import {UserService} from '../../services/user.service';
 
 @Component({
   selector: 'app-user-details',
@@ -13,17 +13,6 @@ import {UserService} from '../../services/user.service';
 export class UserDetailsComponent implements OnInit{
   @Input()
   userDetails: UserInterfaces
-
-  // constructor(private router: Router, private activatedRoute: ActivatedRoute, private userService: UserService) {
-  //   this.activatedRoute.params.subscribe(({id}) => {
-  //     this.userDetails = this.router.getCurrentNavigation()?.extras.state as UserInterfaces;
-  //
-  //     if(!this.userDetails) {
-  //       this.userService.getById(id).subscribe(value => this.userDetails = value)
-  //     }
-  //   })
-  //
-  // }
 
   constructor(private activatedRoute: ActivatedRoute) {
   }
